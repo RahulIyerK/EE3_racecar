f9e8b7de2b4682a435144069571ac0bbc1d963c5
jenseki <jenseki@yahoo.com>
2017-05-11 22:20:41 -0700

    summary
    description
    
    Summary
    
    Description

--- h_bridge_test/h_bridge_test.ino
+++ h_bridge_test/h_bridge_test.ino
@@ -3,21 +3,21 @@
  ******************/
 
 //left motor
-#define LEFT_MOTOR_PINA 11 //direction select pin A
-#define LEFT_MOTOR_PINB 10 //direction select pin B
-#define LEFT_MOTOR_PWM 9   //PWM output pin
+#define LEFT_MOTOR_PINA 3 //direction select pin A
+#define LEFT_MOTOR_PINB 2 //direction select pin B
+#define LEFT_MOTOR_PWM 5   //PWM output pin
 
 //right motor
-#define RIGHT_MOTOR_PINA 8 //direction select pin A
-#define RIGHT_MOTOR_PINB 7 //direction select pin B
-#define RIGHT_MOTOR_PWM 3  //PWM output pin
+#define RIGHT_MOTOR_PINA 7 //direction select pin A
+#define RIGHT_MOTOR_PINB 4 //direction select pin B
+#define RIGHT_MOTOR_PWM 6  //PWM output pin
 
 /********************
  * Value Definitions*
  ********************/
 
-#define FULL_THRUST 255 //full speed is PWM max value (255)
-#define NO_THRUST 0 //zero speed is PWM min value (0)
+#define FULL_THRUST 200 //full speed is PWM max value (255)
+#define NO_THRUST 50 //zero speed is PWM min value (0)
 
 int leftPWMVal = 0; //left motor's PWM Value
 int rightPWMVal = 0; //right motor's PWM Value
@@ -33,6 +33,7 @@ void setup() {
   pinMode (RIGHT_MOTOR_PINB, OUTPUT);
   pinMode (RIGHT_MOTOR_PWM, OUTPUT);
 
+  Serial.begin(38400);
 }
 
 void loop() {
@@ -41,18 +42,23 @@ void loop() {
   digitalWrite(LEFT_MOTOR_PINA, HIGH);
   digitalWrite(LEFT_MOTOR_PINB, LOW);
 
-  digitalWrite(RIGHT_MOTOR_PINA, HIGH);
+  analogWrite(LEFT_MOTOR_PWM, 200);
+  /*digitalWrite(RIGHT_MOTOR_PINA, HIGH);
   digitalWrite(RIGHT_MOTOR_PINB, LOW);
   
   for (int i = NO_THRUST; i <= FULL_THRUST; i++)
   {
     analogWrite(LEFT_MOTOR_PWM, i);
     analogWrite(RIGHT_MOTOR_PWM, i);
+    Serial.println(i);
+    delay(20);
   }
   for (int i = FULL_THRUST; i >= NO_THRUST; i--)
   {
     analogWrite(LEFT_MOTOR_PWM, i);
     analogWrite(RIGHT_MOTOR_PWM, i);
+    Serial.println(i);
+    delay(20);
   }
 
 
@@ -67,11 +73,15 @@ void loop() {
   {
     analogWrite(LEFT_MOTOR_PWM, i);
     analogWrite(RIGHT_MOTOR_PWM, i);
+    Serial.println(i);
+    delay(20);
   }
   for (int i = FULL_THRUST; i >= NO_THRUST; i--)
   {
     analogWrite(LEFT_MOTOR_PWM, i);
     analogWrite(RIGHT_MOTOR_PWM, i);
-  }
+    Serial.println(i);
+    delay(20);
+  }*/
 
 }
